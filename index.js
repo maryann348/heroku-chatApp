090f19b2df595320e428ab8a6ee5b0362816bb5d
camaymaryann123@gmail.com <camaymaryann@123@gmail.com>
2019-09-25 08:34:01 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+var port = process.env.PORT || 3000;
+
+var count = 0;
+
+// var username;
+
+
+//Serve public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, +'public/index.html'));
+});
+
+
+
+io.on('connection', function(socket) {
+    count++;
+    console.log('New User Connected');
+    io.sockets.emit('count', { count: count })
+    socket.on('disconnect', function() {
+        console.log('User Disconnected');
+        count--;
+        io.sockets.emit('count', { count: count })
+    })
+
+    socket.on('message', function(message) {
+        io.emit('message', message);
+    });
+
+    socket.on('typing', function(message) {
+        socket.broadcast.emit('typing', { username: socket.username })
+    })
+    socket.on('connected', function(users) {
+        socket.name = users;
+        io.emit('Connected', users);
+    })
+
+});
+
+http.listen(port, function() {
+    console.log('listening on port 3000');
+});
